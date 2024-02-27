7a4520af0b4f6ac7bffe1177f9be1a7c5f764320
Fayzaaup <rajafaiz628@gmail.com>
2024-02-27 23:19:06 +0700

    Fix Bugs

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,12 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const buyButtons = document.querySelectorAll('.bid a');
 
     buyButtons.forEach(button => {
         button.addEventListener('click', function(event) {
             event.preventDefault();
-
-            const panelName = this.parentElement.parentElement.querySelector('.info h5').innerText;
-            const panelPrice = this.parentElement.parentElement.querySelector('.btc p').innerText;
+        const productName = this.parentElement.parentElement.querySelector('.info h5').innerText;
+        const productPrice = this.parentElement.parentElement.querySelector('.btc p').innerText;
+        const code = this.parentElement.parentElement.querySelector('.bid p').innerText;
+        const typeScript = this.parentElement.parentElement.querySelector('.info p:nth-child(2)').innerText;
             
             const currentDate = new Date();
         const formattedDate = `${currentDate.toLocaleDateString()} ${currentDate.toLocaleTimeString()}`;
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         const note = 'Sertakan screenshot bukti transfer agar admin dapat memproses pesanan anda. Jika tidak ada bukti transfer admin tidak akan memproses pesanan anda walaupun saldo masuk.';
 
-            const whatsappMessage = `𝗡𝗘𝗪 𝗢𝗥𝗗𝗘𝗥 | ${formattedDate}\n\n𝗣𝗥𝗢𝗗𝗨𝗞 : ${panelName}\n𝗣𝗥𝗜𝗖𝗘 : ${panelPrice}\n𝗣𝗔𝗬𝗠𝗘𝗡𝗧 : ${paymentLink}\n\n𝗡𝗼𝘁𝗲: ${note}\n\n*JOIN GRUP WHATSAPP FAYY SHOP*\n${linkGrup}\n\n© 𝗖𝗦 𝗙𝗔𝗬𝗬 𝗦𝗛𝗢𝗣`;
+            const whatsappMessage = `𝗡𝗘𝗪 𝗢𝗥𝗗𝗘𝗥 | ${formattedDate}\n\n𝗣𝗥𝗢𝗗𝗨𝗞 : ${productName}\n𝗣𝗥𝗜𝗖𝗘 : ${productPrice}\n𝗧𝗬𝗣𝗘 𝗖𝗢𝗡𝗡𝗘𝗖𝗧 : ${code}\n𝗧𝗬𝗣𝗘 𝗖𝗢𝗗𝗘 : ${typeScript}\n𝗣𝗔𝗬𝗠𝗘𝗡𝗧 : ${paymentLink}\n\n𝗡𝗼𝘁𝗲: ${note}\n\n*JOIN GRUP WHATSAPP FAYY SHOP*\n${linkGrup}\n\n© 𝗖𝗦 𝗙𝗔𝗬𝗬 𝗦𝗛𝗢𝗣`;
 
             const whatsappLink = `https://wa.me/6281295169487?text=${encodeURIComponent(whatsappMessage)}`;
 
